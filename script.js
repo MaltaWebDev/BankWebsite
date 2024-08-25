9e952056dd7a2ea0fbc5cc5f84ca758bebeb84b5
MaltaWebDev <MaltaWebDev@gmail.com>
2024-08-25 20:42:13 +0200

    Add package.json and script for cookie message

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
+// Modal
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const allSections = document.querySelectorAll('section');
+const allBtns = document.getElementsByTagName('button');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,9 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => {
+  btn.addEventListener('click', openModal);
+});
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +31,36 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//////////////////////////////////////////////
+// Cookie message
+
+// create element and add content
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. By using this website you agree to our cookie policy <button class="btn btn--close-cookie">Got it!</button>';
+const header = document.querySelector('.header');
+
+// add new element to DOM
+header.append(message);
+
+// remove element on btn click
+document.querySelector('.btn--close-cookie').addEventListener('click', () => {
+  message.remove();
+});
+
+// add styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+// message.style.padding = '1rem 0';
+
+// get inline styles
+console.log(message.style.backgroundColor);
+
+// get computed styles if needed
+console.log(getComputedStyle(message).color);
+
+// set height of banner programatically
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
